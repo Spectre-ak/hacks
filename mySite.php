a4bd8b9ac46fb7384322acf1261a81a5387b4487
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-23 14:10:02 +0530

    Update mySite.php

--- mySite.php
+++ mySite.php
@@ -2,7 +2,7 @@
 	
 	//paste the url in the file_get_contents to load the site from your server
 	 
-	$res=file_get_contents("https://www.programiz.com/dsa/binary-search-tree");   
+	$res=file_get_contents("sit-url");   
 	echo $res;
 	
 	
