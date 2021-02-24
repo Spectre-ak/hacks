25efa2fd21eb54f5fee425795afc7dabfc4d92b1
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-25 00:11:59 +0530

    Add files via upload

---
+++ inspectElement/wikiDark.js
@@ -0,0 +1,23 @@
+//dark mode for wikipedia 
+
+document.getElementById("content").style.backgroundColor="black";
+document.getElementById("content").style.color="white";
+
+document.getElementById("mw-head").style.backgroundColor="black";
+document.getElementById("mw-head").style.color="white";
+
+document.getElementById("toc").style.backgroundColor="black";
+document.getElementById("toc").style.color="white";
+
+document.getElementById("mw-panel").style.backgroundColor="black";
+document.getElementById("mw-panel").style.color="white";
+
+document.body.style.backgroundColor="black";
+document.body.style.color="white";
+
+var arr=document.getElementsByTagName("pre");
+for (var i = arr.length - 1; i >= 0; i--) {
+	arr[i].style.backgroundColor="black";
+	arr[i].style.color="white";	
+
+}
