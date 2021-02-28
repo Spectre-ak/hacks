7a9d8f3c26d06a1f1ddce196f772617e398a14dd
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-28 13:31:52 +0530

    removing google iframe ads

---
+++ inspectElement/google_ads_removal.js
@@ -0,0 +1,22 @@
+
+//Removing google ads---
+
+//google renders ads on sites using iframes and below code removes those iframes in regular intervals using setInterval
+
+setInterval(function missionRemoveAds(argument) {
+	var googleAdsIFrame=document.getElementsByTagName("iframe");
+	console.log(googleAdsIFrame);
+	for (var i=0;i<googleAdsIFrame.length; i++) { 
+		try{
+			var ele=googleAdsIFrame[i];
+			ele.remove();
+
+		}
+		catch(err){
+			continue;
+		}
+		
+}
+},1000);
+
+//the interval is set to 1 second, you can decrease it if you want which will help in removing google powered iframes more quickly
