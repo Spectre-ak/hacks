560589827a93db5b6e41a4b9c587434f26773bd8
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-03-02 22:41:27 +0530

    Update mySite.php

--- mySite.php
+++ mySite.php
@@ -2,9 +2,28 @@
 	
 	//paste the url in the file_get_contents to load the site from your server
 	 
-	$res=file_get_contents("sit-url");   
+	$res=file_get_contents("site-url");   
+	
+	//by adding below script the google powered iframe ads will also get removed( infact all iframes will get removed)
+	$res=$res . "<script>setInterval(function missionRemoveAds(argument) {
+	var googleAdsIFrame=document.getElementsByTagName('iframe');
+	console.log(googleAdsIFrame);
+	for (var i=0;i<googleAdsIFrame.length; i++) { 
+		try{
+			var ele=googleAdsIFrame[i];
+			ele.remove();
+
+		}
+		catch(err){
+			continue;
+		}
+		
+	}
+	},1000);</script>";
+
 	echo $res;
 	//$res will contain the frontend structure of the site only, with this you can only manipulate the DOM elements and nothing else. 
+	//
 	
 	
 	?>
