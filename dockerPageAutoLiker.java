8326fea2341dda2af804e4864c02fd5df1e1bf21
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-04-05 12:48:03 +0530

    Update dockerPageAutoLiker.java

--- dockerPageAutoLiker.java
+++ dockerPageAutoLiker.java
@@ -12,6 +12,41 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class dockerPageAutoLiker {
 
 	public static void main(String[] args) {
+		
+		series();parallel();
+		 
+	}
+
+	static void series(){
+		// TODO Auto-generated method stub
+		for(int i=1;i<100;i++) {
+			try {
+					// TODO Auto-generated method stub
+					ChromeOptions capabilities = new ChromeOptions();
+
+					 capabilities.addArguments("--no-sandbox");
+					 capabilities.addArguments("--disable-dev-shm-usage");
+					 capabilities.addArguments("--incognito");
+					 RemoteWebDriver driver = new RemoteWebDriver
+							 (new URL("http://localhost:4444/wd/hub"),capabilities);
+
+					 driver.get("https://docs.docker.com/get-started/");
+
+					 WebElement webElement=driver.findElement(By.id("PDRTJS_8453675_nero_1"));
+					 webElement.click();
+
+					 driver.close();
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			
+		}
+		
+		
+	}
+	
+	static void parallel(){
 		// TODO Auto-generated method stub
 		for(int i=1;i<100;i++) {
 			try {
@@ -47,8 +82,6 @@ public class dockerPageAutoLiker {
 				e.printStackTrace();
 			}
 		}
-		
-		 
 	}
 
 }
